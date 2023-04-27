6fe4687353ed1de8a8575dff929e50d758c1732b
Yahaya Azeez <azconcept.2016@gmail.com>
2023-04-27 14:33:48 +0100

    raise exception message

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def raise_exception():
+    """This function raise TypeError
+    """
+
+    raise TypeError
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """This function raises a name exception with a message.
+
+    Args:
+        message (string): message to print
+    """
+
+    raise NameError(message)
